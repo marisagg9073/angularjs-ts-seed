f63ca4f94f8bf5ac8dbd4ca7c5dfdbe8d73fe27d
Francesco Colamonici <f.colamonici@gmail.com>
2016-03-11 11:00:59 +0100

    fix debug

--- generator/component/temp.module.ts
+++ generator/component/temp.module.ts
@@ -1,4 +1,4 @@
 const ngModuleName = 'app.<%= name %>';
 
 export default angular.module(ngModuleName, ['ngNewRouter'])
-  .run($log => $log.debug(['ngModule', ngModuleName, 'loaded'])).name;
+  .run($log => $log.debug(['ngModule', ngModuleName, 'loaded'].join (' '))).name;
