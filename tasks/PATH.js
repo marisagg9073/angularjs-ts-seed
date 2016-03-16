837c55b3760e11acb222307f2f823445286fc1a1
Francesco Colamonici <f.colamonici@gmail.com>
2016-03-16 16:31:18 +0100

    angular material as dependency for dev and test

--- tasks/PATH.js
+++ tasks/PATH.js
@@ -27,7 +27,11 @@ module.exports = {
       './node_modules/es6-module-loader/dist/es6-module-loader-sans-promises.js.map',
       './node_modules/systemjs/dist/system.src.js',
       './node_modules/angular/angular.js',
-      './node_modules/angular-new-router/dist/router.es5.js'
+      './node_modules/angular-new-router/dist/router.es5.js',
+      './node_modules/angular-aria/angular-aria.js',
+      './node_modules/angular-animate/angular-animate.js',
+      './node_modules/angular-messages/angular-messages.js',
+      './node_modules/angular-material/angular-material.js'
     ],
     blankTemplates: join(__dirname, '..', 'generator', 'component/**/*.**'),
     html: {
