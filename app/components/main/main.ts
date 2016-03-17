059d0a069519dd91a33ac0854f5443874734c1d6
Francesco Colamonici <f.colamonici@gmail.com>
2016-03-17 11:59:08 +0100

    main left sidenav

--- app/components/main/main.ts
+++ app/components/main/main.ts
@@ -1,6 +1,7 @@
 import ngModuleName from './main.module';
 
 // import angular objects in alphabetical order
+import './main-left-sidenav.component';
 import './main-toolbar.component';
 
 export default ngModuleName;
