3ec22979af7729bc798698b9e52111caec6e6716
Francesco Colamonici <f.colamonici@gmail.com>
2016-03-18 10:39:09 +0100

    autocomplete

--- app/components/dashboard/panel/panel.ts
+++ app/components/dashboard/panel/panel.ts
@@ -1,6 +1,7 @@
 import ngModuleName from './panel.module';
 
 // import angular objects in alphabetical order
+import './template/autocomplete.controller';
 import './panel-widget.directive';
 
 export default ngModuleName;
---
+++ app/components/dashboard/panel/template/autocomplete.controller.ts
@@ -0,0 +1,39 @@
+import ngModuleName from '../panel.module';
+
+import {ICountryListItem} from '../../../common/country.model';
+import CountryService from '../../../common/country.service';
+
+'use strict';
+
+const ngControllerName = 'AutocompleteController';
+
+@at.controller(ngModuleName, ngControllerName)
+@at.inject('countryService', '$log', '$q', '$timeout')
+export default class AutocompleteController {
+
+  public countries: Array<ICountryListItem> = [];
+  public selectedCountry: ICountryListItem;
+  public searchText: string;
+  public disableCaching = true;
+
+  constructor(private countryService: CountryService,
+    private log: angular.ILogService,
+    private q: angular.IQService,
+    private timeout: angular.ITimeoutService) {
+    log.debug(['ngController', ngControllerName, 'loaded'].join(' '));
+
+    countryService.loadAllItems().then(countries => this.countries = [].concat(countries));
+  }
+
+  public querySearch(query?: string) {
+    let results = query ? this.countries.filter(this.createFilterFor(query)) : [],
+      deferred = this.q.defer();
+    this.timeout(() => deferred.resolve(results), Math.random() * 1000, false);
+    return deferred.promise;
+  }
+
+  private createFilterFor(query: string) {
+    let lowercaseQuery = angular.lowercase(query);
+    return country => country.value.indexOf(lowercaseQuery) === 0;
+  }
+}
