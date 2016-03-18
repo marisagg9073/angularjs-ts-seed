67ed4ef8afdd1398a484cf0fb86c66ebfd7d2944
Francesco Colamonici <f.colamonici@gmail.com>
2016-03-18 14:22:57 +0100

    panel widget

--- app/components/dashboard/panel/panel-widget.directive.ts
+++ app/components/dashboard/panel/panel-widget.directive.ts
@@ -7,19 +7,19 @@ const ngDirectiveName = 'tsfnPanelWidget';
 @at.directive(ngModuleName, ngDirectiveName, {
   restrict: 'E',
   replace: true,
-  transclude: true,
+  // transclude: true,
   scope: {},
   bindToController: {
     title: '@',
     template: '@',
     options: '@'
   },
-  templateUrl: 'dashboard/panel/panel-widget.directive.html',
-  compile: (element, attrs, linker) => (scope, element) => {
-    linker(scope, clone => {
-      element.append(clone);
-    });
-  }
+  templateUrl: 'dashboard/panel/panel-widget.directive.html'
+  // compile: (element, attrs, linker) => (scope, element) => {
+  //   linker(scope, clone => {
+  //     element.append(clone);
+  //   });
+  // }
 })
 export default class PanelWidgetDirective {
 }
