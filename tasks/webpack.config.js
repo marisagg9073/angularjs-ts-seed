8986f728494f3b6d54da3e313752a050ce65de83
Francesco Colamonici <f.colamonici@gmail.com>
2016-03-21 17:03:43 +0100

    loaders

--- tasks/webpack.config.js
+++ tasks/webpack.config.js
@@ -1,22 +1,36 @@
 var path = require('path');
 var webpack = require('webpack');
 
+// https://webpack.github.io/docs/code-splitting.html
+var angular = ['aria', 'animate', 'messages', 'material',
+  'ui-router', 'cookies', 'sanitize', 'nvd3'].map(function(dep) {
+    return 'angular-' + dep;
+  });
 module.exports = {
   entry: {
+    angular: ['angular'].concat(angular),
+    vendor: ['d3'],
+    at: './app/at-angular*.ts',
     app: './app/app.ts',
-    at: './app/at-angular.ts'
   },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, '..', 'dist'),
     publicPath: '/dev/',
   },
   // devtool: 'inline-source-map',
   resolve: {
-    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js'],
+    // alias: {
+    //   at: 'at.bundle.js'
+    // }
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin()
+    // new webpack.optimize.CommonsChunkPlugin('common.js'),
+    // new webpack.optimize.CommonsChunkPlugin({ names: ['at', 'angular', 'vendor'] }),
+    // new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
+    // new webpack.optimize.CommonsChunkPlugin('angular', 'angular.bundle.js')
+    // new webpack.optimize.UglifyJsPlugin()
   ],
   module: {
     loaders: [
@@ -26,7 +40,8 @@ module.exports = {
           path.resolve(__dirname, '..', 'app')
         ],
         loader: 'ts-loader'
-      }
+      },
+      { test: /[\/]angular\.js$/, loader: 'exports?angular' }
     ]
   }
 };
