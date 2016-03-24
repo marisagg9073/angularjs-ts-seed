f86e00b7b717c94a97ee4a78995282d16685df02
Francesco Colamonici <f.colamonici@gmail.com>
2016-03-24 17:14:27 +0100

    showcase lazy-loading of source code files (ux enhanced)

--- app/components/showcase/showcase.component.ts
+++ app/components/showcase/showcase.component.ts
@@ -20,7 +20,7 @@ const ngComponentName = 'tsfnShowcase';
   },
   templateUrl: 'showcase/showcase.component.html'
 })
-@at.inject('showcase', '$log', '$q')
+@at.inject('showcase', '$log', '$q', '$timeout', '$mdDialog')
 export default class ShowcaseComponent implements at.OnInit {
   public fileList: string[];
   public lazy: boolean;
@@ -41,7 +41,9 @@ export default class ShowcaseComponent implements at.OnInit {
 
   constructor(private showcase: ShowcaseService,
     private log: angular.ILogService,
-    private q: angular.IQService) {
+    private q: angular.IQService,
+    private timeout: angular.ITimeoutService,
+    private mdDialog: angular.material.IDialogService) {
     log.debug(['ngComponent', ngComponentName, 'loaded'].join(' '));
   }
 
@@ -50,24 +52,48 @@ export default class ShowcaseComponent implements at.OnInit {
       this.load();
   }
 
-  public toggleSource() {
-    this.load().then(loaded => loaded ? this.toggleSourceInternal() : this.q.reject());
+  public toggleSource($event) {
+    this.load($event).then(loaded => loaded ? this.toggleSourceInternal() : this.q.reject());
   }
 
   private toggleSourceInternal() {
     this.showSource = !this.showSource;
   }
 
-  private load() {
+  private load($event?: PointerEvent) {
     if (!this.loaded) {
       // this.fileList.push('components/showcase/showcase.scss');
-      return this.showcase.load(this.fileList).then(files => {
-        for (let path in files) {
-          this.tabs.push(this.fileToTab(path, files[path]));
-        }
-      }).then(() => this.loaded = true);
+      this.showLoadingAlert($event);
+      return this.showcase.load(this.fileList)
+        .then(files => {
+          for (let path in files) {
+            this.tabs.push(this.fileToTab(path, files[path]));
+          }
+        })
+        .then(() => this.loaded = true)
+        .then(() => this.hideLoadingAlert());
     } else
       return this.q.when(true);
+  }
+
+  private showLoadingAlert($event: PointerEvent) {
+    let alert = this.mdDialog.alert()
+      .title('Loading')
+      .textContent('Fecthing source code files...')
+      .ok('Ok')
+      .targetEvent($event);
+    this.mdDialog
+      .show(alert)
+      .finally(() => {
+        alert = undefined;
+      });
+  }
+
+  private hideLoadingAlert() {
+    let deferred = this.q.defer();
+    let delay = 200 + 15 * this.fileList.length;
+    this.timeout(() => deferred.resolve(this.mdDialog.hide(true)), delay);
+    return deferred.promise;
   }
 
   private fileToTab(path: string, content: string): ITab {
