c950f636c56ec6869e1ff3ec971a0b4353362d26
Francesco Colamonici <f.colamonici@gmail.com>
2016-03-25 10:44:52 +0100

    ngMessages included by default in Material module

--- app/components/material/material.module.ts
+++ app/components/material/material.module.ts
@@ -4,6 +4,6 @@ import cfg from './material.config';
 
 const ngModuleName = 'app.components.material';
 
-export default angular.module(ngModuleName, ['ngComponentRouter', 'ngMaterial'])
+export default angular.module(ngModuleName, ['ngComponentRouter', 'ngMaterial', 'ngMessages'])
   .config(cfg)
   .run(['$log', $log => $log.debug(['ngModule', ngModuleName, 'loaded'].join(' '))]).name;
